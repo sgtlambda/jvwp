73e769c14dfe502d830c101ed0867eca2005b7e8
JM Versteeg <janmerijnversteeg@gmail.com>
2014-11-21 15:17:06 +0100

    improve autoloader performance

--- autoload.php
+++ autoload.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace jvwp;
-
-spl_autoload_register(function($className) {
-    if(strpos($className, __NAMESPACE__) === 0) {
-        $target = dirname(__DIR__) . '/src/' . $className . '.php';
-        if (file_exists($target))
-            include $target;
-    }
+spl_autoload_register(function ($class) {
+    $prefix = 'jvwp\\';
+    $base_dir = __DIR__ . '/src/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0)
+        return;
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file))
+        require $file;
 });
