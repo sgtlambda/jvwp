8d5b9a8e81807838322fb1091d888bb9b8d58968
JM Versteeg <janmerijnversteeg@gmail.com>
2014-11-21 15:14:35 +0100

    rename class files and add generic autoloader

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@ namespace jvwp;
 
 spl_autoload_register(function($className) {
     if(strpos($className, __NAMESPACE__) === 0) {
-        $target = dirname(__DIR__) . '/' . $className . '.class.php';
+        $target = dirname(__DIR__) . '/src/' . $className . '.php';
         if (file_exists($target))
             include $target;
     }
