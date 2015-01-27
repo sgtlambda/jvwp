1eb6e2b41475e451e93369f685e140bf43900c1b
jmversteeg <janmerijnversteeg@gmail.com>
2015-01-27 10:30:07 +0100

    Add TextField class

---
+++ src/metaboxes/fields/TextField.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace jvwp\metaboxes\fields;
+
+class TextField extends Field
+{
+
+    const TYPE_TEXT     = 'text';
+    const TYPE_PASSWORD = 'password';
+
+    private $type;
+
+    public function __construct ($identifier, $label, $type = self::TYPE_TEXT, $default = "")
+    {
+        parent::__construct($identifier, $label, $default);
+        $this->type = type;
+    }
+
+    /**
+     * Displays the field control with given value
+     *
+     * @param string $value
+     */
+    protected function output ($value)
+    {
+        echo "<input type=\"{$this->type}\" value=\"{$value}\">";
+    }
+}
