457c080a94bb261452cdc36c99c3bc067bf45fcd
jmversteeg <janmerijnversteeg@gmail.com>
2015-03-20 16:09:47 +0100

    Fix image functionality

--- src/common/Image.php
+++ src/common/Image.php
@@ -28,9 +28,48 @@ class Image
             return $default;
     }
 
+    /**
+     * Resolves URL to a path accessible through the local file system
+     *
+     * @param $imageUrl
+     *
+     * @return string
+     */
+    private static function getPathByUrl ($imageUrl)
+    {
+        $contentUrl = content_url();
+        if (strpos($imageUrl, '/') === 0)
+            $imageUrl = self::getRootUrl() . $imageUrl;
+        return WP_CONTENT_DIR . str_replace($contentUrl, '', $imageUrl);
+    }
+
+    /**
+     * Gets the WordPress home root URL
+     * @return string
+     */
+    private static function getRootUrl ()
+    {
+        if (defined('WP_HOME_ROOT'))
+            return WP_HOME_ROOT;
+        else
+            return home_url();
+    }
+
+    /**
+     * Gets a relative URL to the post thumbnail, scaled down to the given size
+     *
+     * @param        $w
+     * @param        $h
+     * @param int    $pixelRatio
+     * @param null   $post_id
+     * @param string $size
+     * @param string $default
+     *
+     * @return string
+     */
     public static function getScaledThumbnail ($w, $h, $pixelRatio = 1, $post_id = null, $size = 'full', $default = '')
     {
-        $path = self::getThumbnail($post_id, $size, '');
+        $path = self::getPathByUrl(self::getThumbnail($post_id, $size, ''));
         if ($path === '')
             return $default;
         return self::getScaledImageUrl($path, $w, $h, $pixelRatio);
@@ -40,8 +79,9 @@ class Image
     {
         $realW = $w * $pixelRatio;
         $realH = $h * $pixelRatio;
-        return '/' . \Gregwar\Image\Image::open($path)
-            ->zoomCrop($realW, $realH, 0xfffff)
-            ->jpeg(90);
+        $path = '/' . \Gregwar\Image\Image::open($path)
+                ->zoomCrop($realW, $realH, 0xfffff)
+                ->jpeg(90);
+        return $path;
     }
 }
