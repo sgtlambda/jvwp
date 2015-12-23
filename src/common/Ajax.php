8e91d28f93d15e5fd6a627d997e76a0212704bcd
JM Versteeg <janmerijnversteeg@gmail.com>
2015-12-23 11:41:33 +0100

    Add nopriv functionality

--- src/common/Ajax.php
+++ src/common/Ajax.php
@@ -5,32 +5,44 @@ namespace jvwp\common;
 class Ajax
 {
 
+    const REGISTER_BOTH = 'both';
+
     /**
      * The hook prefix
      */
     const WP_AJAX_HOOK_PREFIX = 'wp_ajax_';
 
     /**
+     * The nopriv (not logged in) prefix
+     */
+    const WP_AJAX_NOPRIV_HOOK_PREFIX = 'wp_ajax_nopriv_';
+
+    /**
      * Gets the action hook string to use, based on the action name that was provided
      *
      * @param string $action
+     * @param bool   $noPriv
      *
      * @return string
      */
-    public static function getHook ($action)
+    public static function getHook ($action, $noPriv = false)
     {
-        return self::WP_AJAX_HOOK_PREFIX . $action;
+        return ($noPriv ? self::WP_AJAX_NOPRIV_HOOK_PREFIX : self::WP_AJAX_HOOK_PREFIX) . $action;
     }
 
     /**
      * Binds the callable to the given hook
      *
-     * @param string   $action   The name of the ajax action
-     * @param callable $callable The function to bind
+     * @param string      $action   The name of the ajax action
+     * @param callable    $callable The function to bind
+     * @param bool|string $noPriv   Whether to enable nopriv in the hook
      */
-    public static function register ($action, $callable)
+    public static function register ($action, $callable, $noPriv = false)
     {
-        add_action(self::getHook($action), $callable);
+        if (!$noPriv || $noPriv === self::REGISTER_BOTH)
+            add_action(self::getHook($action), $callable);
+        if ($noPriv || $noPriv === self::REGISTER_BOTH)
+            add_action(self::getHook($action, true), $callable);
     }
 
     /**
